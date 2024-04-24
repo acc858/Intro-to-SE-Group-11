2496a5bf3f12490aa318d62a82f24d6aa2add7b5
Aden Carter <157550210+acc858@users.noreply.github.com>
2024-04-23 23:43:58 -0500

    Delete home directory

--- home/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import listing, Seller, Buyer
-# Register your models here.
-admin.site.register(Seller)
-admin.site.register(Buyer)
-admin.site.register(listing)
